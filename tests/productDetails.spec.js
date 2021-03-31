11af6dca6c11d4feacd41f3e4035b5055ee46676
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-03-31 18:45:22 -0300

    correções requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -37,12 +37,14 @@ describe('#productDetails', () => {
     // Teste que o retorno da função é um array.
     assert.strictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.strictEqual(Object.keys(productDetails()).length,2);
+    assert.strictEqual(Object.keys(productDetails()).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     assert.strictEqual(typeof productDetails()[0], 'object');
     // Teste que os dois objetos são diferentes entre si.
     assert.notDeepStrictEqual(productDetails()[0] && productDetails()[1], 'true');
     // (Difícil) Teste que os dois productIds terminam com 123.
-    assert.strictEqual(productDetails()[0].details.productId.endsWith('123'), productDetails()[1].details.productId.endsWith('123'),true);
+    const firstProduct = productDetails()[0].details.productId;
+    const secondProduct = productDetails()[1].details.productId;
+    assert.strictEqual(firstProduct.endsWith('123'), secondProduct.endsWith('123'), true);
   });
 });
